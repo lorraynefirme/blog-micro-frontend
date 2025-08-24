88cd5b1c09cafdac466e1eceb273e96c63283a54
Lorrayne Firme <lorraynefirme@gmail.com>
2025-08-24 19:08:06 -0300

    feat: componente footer

---
+++ packages/ui/src/components/Footer/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="border-t border-zinc-200/70 text-center text-xs text-brand-50 fixed bottom-0 left-0 w-full py-4">
+      <div>© {new Date().getFullYear()} Micro-frontend Multizone</div>
+    </footer>
+  );
+};
